2b2cf66a4ce7977295ff42f273faa867ced9e49c
ziyue <ziyue@moonlyt.com>
2014-01-15 16:39:36 -0500

    max depth of binary tree

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-1.Single Number:
+1.Single Number
+
 Given an array of integers, every element appears twice except for one. Find that single one.
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+2.Maximum Depth of Binary Tree
+
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
---
+++ maxDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+public class maxDepthOfBinaryTree {
+	public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        else {
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+         }
+    }
+    // 			a
+    //		   / \
+    //        b   c
+    //       / \ 
+    //      d   e
+    //     /
+    //    f
+    public static void main(String[] args){
+    	TreeNode a = new TreeNode(1);
+    	TreeNode b = new TreeNode(1);
+    	TreeNode c = new TreeNode(1);
+    	TreeNode d = new TreeNode(1);
+    	TreeNode e = new TreeNode(1);
+    	TreeNode f = new TreeNode(1);
+    	a.left = b;
+    	a.right = c;
+    	b.left = d;
+    	b.right = e;
+    	d.left = f;
+    	maxDepthOfBinaryTree mdbt = new maxDepthOfBinaryTree();
+    	System.out.println(mdbt.maxDepth(a));
+
+    }
+}
